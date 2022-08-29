4a2d9e832b846e80223649c225dd5f557d18c0f4
Hiresh <hbremanand4@gmail.com>
2022-08-29 16:11:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'; 
+import { Dots, ProgressBar, Waves, Spinner, TrinitySpinner, MinimalSpinner } from 'loading-animations-react';
 
 function App() {
+  const [shouldRender, changeState] = useState(false)
+
+  const el = setTimeout(()=>{
+    changeState(true)
+  }, 5000)
+
+  if(shouldRender===true) {
+    return(<h1 className='text-center text-[40px] underline mt-20'>This is the rendered page!</h1>)
+  } else{
+    return(
+    <div className='absolute w-[100%] h-[100%] bg-blue-200'>
+      <Dots text="" className="absolute w-[100px] left-[45%] right-[50%] top-[40%]" dotColors={[ 'red', 'white', 'blue', '#123abc', 'rgb(50,50,50)','hsla(235, 100%, 50%, .5)' ]} />
+    </div>)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></> 
   );
 }
 
